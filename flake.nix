68cafbe0f0b7012cf12476043cf2cbbe17751779
Lychee <itslychee@protonmail.com>
2023-10-06 01:06:05 -0500

    flake.nix: stick with the latest Go package

--- flake.nix
+++ flake.nix
@@ -1,18 +1,27 @@
 {
-    inputs.utils.url = "github:numtide/flake-utils";
-    inputs.gomod2nix.url = "github:nix-community/gomod2nix";
-    outputs = { self, nixpkgs, utils, gomod2nix, ...}@inputs: utils.lib.eachDefaultSystem (system: 
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    unstable-nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    gomod2nix.url = "github:nix-community/gomod2nix";
+  };
+  outputs = { self, nixpkgs, unstable-nixpkgs, utils, gomod2nix, ... }@inputs: utils.lib.eachDefaultSystem (system:
     let
-        pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ gomod2nix.overlays.default ];
-        }; 
-    in with pkgs; {
-        devShells.default = mkShell {
-            packages = [ go gomod2nix.packages.${system}.default ];
-        };
-        # Making it accessible to other Nix users
-        packages.default =  import nix/packages.nix pkgs;
-        overlays.default = (_: _: { mpdrp = self.packages.${system}.default.mpdrp; });
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          gomod2nix.overlays.default
+          (final: prev: {
+            unstable = unstable-nixpkgs.legacyPackages.${system};
+          })
+        ];
+      };
+    in
+    with pkgs; {
+      devShells.default = mkShell {
+        packages = [ unstable.go gomod2nix.packages.${system}.default ];
+      };
+      # Making it accessible to other Nix users
+      packages.default = import nix/packages.nix pkgs;
+      overlays.default = (_: _: { mpdrp = self.packages.${system}.default.mpdrp; });
     });
 }
