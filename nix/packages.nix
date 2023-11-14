10aa3a19e911f0cb51de0e181b513eb3e3e2094b
lychee <itslychee@protonmail.com>
2023-11-14 08:56:38 -0600

    Add mpc subpackage

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,16 +1,26 @@
 pkgs:
 with pkgs;
 let 
-    drvTemplate = name: output: buildGoApplication {
-        inherit name;
+
+in rec {
+    mpdrp = buildGoApplication rec {
+        name = "mpdrp";
         pname = name;
         go = pkgs.go;
         modules = ./gomod2nix.toml;
         src = ../.;
         subPackages = [
-            "cmd/${name}"
+            "cmd/mpdrp"
         ];
     };
-in rec {
-    mpdrp = drvTemplate "mpdrp" "mpdrp";
+    mpdrp-mpc = buildGoApplication rec {
+        name = "mpdrp-mpc";
+        pname = name;
+        go = pkgs.go;
+        modules = ./gomod2nix.toml;
+        src = ../.;
+        subPackages = [
+            "cmd/mpc"
+        ];
+    };
 } 
