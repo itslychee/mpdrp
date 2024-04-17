c8163bcaceb219f6279c057b3058aef87990c46b
itslychee <itslychee@protonmail.com>
2024-04-16 20:36:34 -0500

    add stability notice

--- default.nix
+++ default.nix
@@ -4,6 +4,8 @@ rec {
         name = "mpdrp";
         vendorHash = "sha256-LcYXXlHPHze9zWpJ6wB5o0py/wVzYW0r2m7liJF0uWg=";
     };
+    # this is mostly for personal usage, I do not guarantee
+    # stability with this.
     mpdrp-mpc = pkgs.callPackage ./nix/package.nix {
         name = "mpc";
         vendorHash = "sha256-LcYXXlHPHze9zWpJ6wB5o0py/wVzYW0r2m7liJF0uWg=";
