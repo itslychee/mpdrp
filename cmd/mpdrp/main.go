aa41fe35fc5fd4ea52b80f8657029b7065cf0382
itslychee <itslychee@protonmail.com>
2024-04-16 20:15:03 -0500

    mpdrp: remove activity on a 5 minutes of inactivity on a paused state

--- cmd/mpdrp/main.go
+++ cmd/mpdrp/main.go
@@ -27,19 +27,73 @@ func init() {
 	flag.Parse()
 }
 
-// retry_or_panic sucks but I intend to package mpd and discord
+func formatActivity(activity *discord.Activity, currentStatus *music.Response, cachedURLs map[string]string) {
+	if currentStatus.Get("state") != "stop" {
+		// Activity metadata
+		album, ok := currentStatus.Records["Album"]
+		if !ok {
+			album = []string{"??"}
+		}
+		artist, ok := currentStatus.Records["Artist"]
+		if !ok {
+			artist = []string{"??"}
+		}
+		details, ok := currentStatus.Records["Title"]
+		if !ok {
+			details = []string{"??"}
+		}
+
+		state := fmt.Sprintf("%s by %s", album[0], artist[0])
+		activity.Details = &details[0]
+		activity.State = &state
+
+		var err error
+
+		// Reuse last image if available
+		v, ok := cachedURLs[currentStatus.Get("songid")]
+		if !ok {
+			v, err = GetCoverArt(*currentStatus)
+			if err != nil {
+				logf(Normal, "[error] encountered error while fetching cover art: %s", err)
+			} else {
+				cachedURLs[currentStatus.Get("songid")] = v
+			}
+		}
+		activity.Assets.LargeImage = v
+		activity.Assets.SmallImage = PauseAsset
+		activity.Assets.SmallText = "Paused"
+		if currentStatus.Get("state") == "play" {
+			activity.Assets.SmallImage = PlayAsset
+			activity.Assets.SmallText = "Playing"
+			var duration, elapsed float64
+			if duration, err = strconv.ParseFloat(currentStatus.Get("duration"), 64); err != nil {
+				panic(err)
+			}
+			if elapsed, err = strconv.ParseFloat(currentStatus.Get("elapsed"), 64); err != nil {
+				panic(err)
+			}
+			activity.Timestamps = &discord.Timestamps{
+				End: time.Now().Add(time.Second * time.Duration(duration-elapsed)).Unix(),
+			}
+		}
+	} else {
+		activity = nil
+	}
+
+}
+
+// retryOrScream sucks but I intend to package mpd and discord
 // into their own full fledge repos some day :tm:
 //
 // function should not be called if it is out of reconnection context
 // and the program can recover from this error by reconnecting
-func retry_or_panic(err error) {
+func retryOrScream(err error) {
 	handledErrors := []syscall.Errno{
 		syscall.ECONNREFUSED,
 		syscall.ECONNABORTED,
 		syscall.ECONNRESET,
 		syscall.EPIPE,
 	}
-
 
 	// Get syscall errno hex
 	if v, ok := err.(*net.OpError); ok {
@@ -55,17 +109,21 @@ func retry_or_panic(err error) {
 		io.EOF,
 	}
 
+
+        // handle generic errors
 	for _, v := range handledErrs {
 		if errors.Is(err, v) {
+			log(Network, "reattempting connection")
+			time.Sleep(5 * time.Second)
 			return
 		}
 	}
 
-
+        // handle specific socket errors
 	for _, e := range handledErrors {
 		if errors.Is(err, e) {
 			log(Network, "reattempting connection")
-			time.Sleep(3 * time.Second)
+			time.Sleep(5 * time.Second)
 			return
 		}
 	}
@@ -73,14 +131,23 @@ func retry_or_panic(err error) {
 	panic(err)
 }
 
+type Signal uint8
+
+const (
+	Play = iota
+	Pause
+	Stopped
+	Retry
+)
+
 func main() {
 conn:
 	client := discord.NewDiscordPresence(strconv.Itoa(*clientID))
 	mpd := new(music.MPDConnection)
 	log(Normal, "connecting to discord")
 	if err := client.Connect(); err != nil {
-		logf(Normal, "failed %s", err)
-		retry_or_panic(err)
+		logf(Normal, "failed: %s", err)
+		retryOrScream(err)
 		goto conn
 	}
 	defer client.Close()
@@ -90,96 +157,94 @@ conn:
 	logjson(Network, "handshake result", json.RawMessage(b))
 	if err != nil {
 		logf(Normal, "failed %s", err)
-		retry_or_panic(err)
+		retryOrScream(err)
 		goto conn
 	}
 	log(Normal, "connecting to mpd")
 	if err := mpd.Connect(nil); err != nil {
 		logf(Normal, "failed %s", err)
-		retry_or_panic(err)
+		retryOrScream(err)
 		goto conn
 	}
 	defer mpd.Close()
 	logf(Normal, "connected to mpd instance: %s", mpd.RawConn.RemoteAddr())
 
 	var cachedURLs = make(map[string]string)
+
+	var lastSignal Signal
+	ch := make(chan Signal, 1)
+
+	go func() {
+		for {
+			// Get current status of song
+			currentStatus, err := mpd.Exec(
+				music.Command{Name: "currentsong"},
+				music.Command{Name: "status"},
+			)
+			if err != nil {
+				retryOrScream(err)
+				ch <- Retry
+				return
+			}
+
+			var activity *discord.Activity
+
+			if currentStatus.Records["state"][0] != "stop" {
+				activity = &discord.Activity{
+					Assets: &discord.Assets{LargeText: "Music Player Daemon"},
+				}
+			}
+
+			formatActivity(activity, currentStatus, cachedURLs)
+
+			_, body, err := client.SetActivity(activity)
+			logjson(Network, "set activity", json.RawMessage(body))
+			if err != nil {
+				retryOrScream(err)
+				ch <- Retry
+				return
+			}
+
+			switch currentStatus.Records["state"][0] {
+			case "play":
+				ch <- Play
+			case "pause":
+				ch <- Pause
+			case "stop":
+				ch <- Stopped
+			}
+			// Idle and wait
+			_, err = mpd.Exec(music.Command{Name: "idle", Args: []string{"player"}})
+			if err != nil {
+				retryOrScream(err)
+				ch <- Retry
+				return
+			}
+		}
+
+	}()
 	for {
-		// Get current status of song
-		currentStatus, err := mpd.Exec(
-			music.Command{Name: "currentsong"},
-			music.Command{Name: "status"},
-		)
-		if err != nil {
-			retry_or_panic(err)
-			goto conn
-		}
 
-		var activity = &discord.Activity{
-			Assets: &discord.Assets{
-				LargeText: "Music Player Daemon",
-			},
-		}
-		if currentStatus.Get("state") != "stop" {
-			// Activity metadata
-			album, ok := currentStatus.Records["Album"]
-			if !ok {
-				album = []string{"??"}
-			}
-			artist, ok := currentStatus.Records["Artist"]
-			if !ok {
-				artist = []string{"??"}
-			}
-			details, ok := currentStatus.Records["Title"]
-			if !ok {
-				details = []string{"??"}
+		select {
+		case <-time.After(time.Minute * 5):
+
+			if lastSignal != Pause {
+				continue
 			}
 
-			state := fmt.Sprintf("%s by %s", album[0], artist[0])
-			activity.Details = &details[0]
-			activity.State = &state
+			_, body, err := client.SetActivity(nil)
+			logjson(Network, "paused for too long, timing out", json.RawMessage(body))
+			if err != nil {
+				retryOrScream(err)
 
-			// Reuse last image if available
-			v, ok := cachedURLs[currentStatus.Get("songid")]
-			if !ok {
-				v, err = GetCoverArt(*currentStatus)
-				if err != nil {
-					logf(Normal, "[error] encountered error while fetching cover art: %s", err)
-				} else {
-					cachedURLs[currentStatus.Get("songid")] = v
-				}
 			}
-			activity.Assets.LargeImage = v
-			activity.Assets.SmallImage = PauseAsset
-			activity.Assets.SmallText = "Paused"
-			if currentStatus.Get("state") == "play" {
-				activity.Assets.SmallImage = PlayAsset
-				activity.Assets.SmallText = "Playing"
-				var duration, elapsed float64
-				if duration, err = strconv.ParseFloat(currentStatus.Get("duration"), 64); err != nil {
-					panic(err)
-				}
-				if elapsed, err = strconv.ParseFloat(currentStatus.Get("elapsed"), 64); err != nil {
-					panic(err)
-				}
-				activity.Timestamps = &discord.Timestamps{
-					End: time.Now().Add(time.Second * time.Duration(duration-elapsed)).Unix(),
-				}
-			}
-		} else {
-			activity = nil
-		}
-		_, body, err := client.SetActivity(activity)
-		logjson(Network, "set activity", json.RawMessage(body))
-		if err != nil {
-			retry_or_panic(err)
-			goto conn
-		}
-		// Idle and wait
-		_, err = mpd.Exec(music.Command{Name: "idle", Args: []string{"player"}})
-		if err != nil {
-			retry_or_panic(err)
-			goto conn
-		}
 
+		case sig := <-ch:
+			lastSignal = sig
+			if sig == Retry {
+				goto conn
+			}
+
+		}
 	}
 }
