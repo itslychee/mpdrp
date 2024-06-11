1a4f58b5aad1e0a9ac1674bb00587913044ca1b6
itslychee <itslychee@protonmail.com>
2024-06-11 12:37:18 -0500

    flake.nix: remove superfluous system in withSystems

--- flake.nix
+++ flake.nix
@@ -2,24 +2,29 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
-  outputs = { self, nixpkgs, }: let
-    supportedSystems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
     withSystems = f:
-      nixpkgs.lib.genAttrs supportedSystems (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-        f {inherit system pkgs;});
+      nixpkgs.lib.genAttrs
+      supportedSystems
+      (system: f nixpkgs.legacyPackages.${system});
   in {
-    packages = withSystems ({
-      pkgs,
-      system,
-    }: import ./. { inherit pkgs; });
-    formatter = withSystems ({ pkgs, system, }: pkgs.alejandra);
+    packages = withSystems (pkgs: import ./. {inherit pkgs;});
+    formatter = withSystems (pkgs: pkgs.alejandra);
     homeManagerModules.default = import ./nix/module.nix self;
-    devShell = withSystems ({ pkgs, system}: pkgs.mkShell {
+    devShell = withSystems (pkgs:
+      pkgs.mkShell {
         packages = builtins.attrValues {
-            inherit (pkgs) gopls go;
+          inherit (pkgs) gopls go;
         };
-    });
+      });
   };
 }
